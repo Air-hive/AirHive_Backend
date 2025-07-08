8fa0e7bac1a58bc801045a01196b3e7d1e52657c
Mohamed Saad <mo.sa3d234@gmail.com>
2025-07-08 15:06:37 +0300

    Fixing Status return

--- PrinterToBackend.py
+++ PrinterToBackend.py
@@ -1,3 +1,5 @@
+import time
+
 import requests
 import json
 import mdns
@@ -13,7 +15,6 @@ def send_commandd_to_printer(printer_ip, commands):
             json={"commands": commands},
             timeout=5
         )
-
         if response.status_code == 200:
             return jsonify(response.json()),200
         else:
@@ -30,14 +31,16 @@ def get_responses_from_printer(printer_ip, size):
         response = requests.get(
             f"http://{printer_ip}/responses",
             json={"size": size},
-            timeout=5
+            timeout=10
         )
+
         if response.status_code == 200:
             return jsonify(response.json()),200
         else:
             return jsonify({"error": f"Printer returned {response.status_code}"}), 500
 
     except Exception as e:
+        print(f"Printer returned: from exception: {e}",printer_ip)
         return jsonify({"error": str(e)}), 500
 
 def config_printer(printer_ip, baudrate):
@@ -50,7 +53,6 @@ def config_printer(printer_ip, baudrate):
             json={"baudrate": baudrate},
             timeout=5
         )
-
         if response.status_code == 200:
             return jsonify(response.json()), 200
         else:
